a8b0017045124f26a1c4536f2d127cd1100dcadb
nezorepla <alperozen84@gmail.com>
2017-10-11 17:26:01 +0300

    Update fx_fiil_detay.sql

--- fx_fiil_detay.sql
+++ fx_fiil_detay.sql
@@ -1,25 +1,18 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
--- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
-create  FUNCTION [dbo].[fx_fiil_detay]
+ALTER  FUNCTION [dbo].[fx_fiil_detay]
 (
   @gelen varchar(50) 
 )
 RETURNS 
   @Table_Var  TABLE
 (
-   gelen varchar(50) ,
-  c1 varchar(1),
-    c2 varchar(1),
-	c3 bit,
-	c4 bit,c5 varchar(1),c6 varchar(1)
+	gelen varchar(50) ,
+	SonSesli varchar(1),
+	SonHarf varchar(1),
+	SessizIleBitiyorFlg bit,
+	SertSessizIleBitiyorFlg bit,
+	DuzGenisSesliHarf varchar(1),
+	DarSesliHarf varchar(1)
+	 
 )   with schemabinding
 AS
 BEGIN
@@ -43,17 +36,19 @@ set @SonHarf =RIGHT(@gelen,1) ;
 set @SessizIleBitiyorFlg= case when @SonHarf in ('i','e','ı','i','a','u','ü','o','ö') then 0 else 1 end ;
 set @SertSessizIleBitiyorFlg= case when @SonHarf in ('f','s','t','k','ç','ş','h','p') then 1 else 0 end ;
  
- set @DuzGenisSesliHarf = case when @SonSesli in ('a','ı','o','u') then 'a' else 'e' end
+ set @DuzGenisSesliHarf = case when @SonSesli in ('a','ı','o','u') then 'a' else 'e' end;
 
- set @DarSesliHarf= case @SonSesli 
- when  'e' then 'i' 
- when  'a' then 'ı' 
- when  'o' then 'u' 
- when  'ö' then 'ü' 
- when  'i' then 'e' 
+ set @DarSesliHarf= case
+ when  @SonSesli  ='e' then 'i' 
+ when  @SonSesli  ='a' then 'ı' 
+ when  @SonSesli  ='o' then 'u' 
+ when  @SonSesli  ='ö' then 'ü' 
+
+--when @SonSesli  = 'ü' then 'i' 
+ --when @SonSesli  = 'i' then '' 
  -- when  'u' then 'x' 
  else @SonSesli
-  end
+  end;
 
  insert into @Table_Var values (@gelen,@SonSesli,@SonHarf,@SessizIleBitiyorFlg,@SertSessizIleBitiyorFlg,@DuzGenisSesliHarf,@DarSesliHarf)	
 	RETURN  
